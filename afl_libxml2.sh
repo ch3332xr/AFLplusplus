4ca415317a6efbfa0b15565445862ae1bbc2eed1
xcH3332 <brush123z@gmail.com>
2020-06-16 22:44:05 +0530

    Update afl_libxml2.sh

--- afl_libxml2.sh
+++ afl_libxml2.sh
@@ -9,10 +9,10 @@ echo -e "[+] Enabling Sanitizers"
 export AFL_USE_UBSAN=1
 export AFL_USE_ASAN=1
 echo -e "[+] Building the lib"
-make CC=~/AFLplusplus/afl-clang-fast CXX=~/AFLplusplus/afl-clang-fast++ LD=~/AFLplusplus/afl-clang-fast
+make CC=~/AFLplusplus/AFLplusplus/afl-clang-fast CXX=~/AFLplusplus/AFLplusplus/afl-clang-fast++ LD=~/AFLplusplus/AFLplusplus/afl-clang-fast
 echo -e "[+] Creating I/O dirs....Adding seeds"
 mkdir fuzz ; cp xmllint fuzz/xmllint_cov ; mkdir fuzz/in ; cp test/*.xml fuzz/in/ ; cd fuzz
 echo -e "[+] Configuring Sys 4 AFL++"
 sudo ~/AFLplusplus/afl-system-config
 echo -e "[+] GO fUZZ!!"
-~/AFLplusplus/afl-fuzz -i in/ -o out -m none -d -- ./xmllint_cov @@
+~/AFLplusplus/AFLplusplus/afl-fuzz -i in/ -o out -m none -d -- ./xmllint_cov @@
