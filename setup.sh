1837168d033701d849a3f603ff2f53e0e8f2e83b
xcH3332 <brush123z@gmail.com>
2020-06-16 16:19:09 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo -e "[+] Installing dependencies"
+sudo apt update
+sudo apt upgrade -y
+sudo apt install build-essential libtool-bin python3-dev automake flex bison libglib2.0-dev libpixman-1-dev clang python3-setuptools llvm
+echo -e "[+] Cloning AFLplusplus"
+git clone https://github.com/AFLplusplus/AFLplusplus
+cd AFLplusplus
+echo -e "[+] Building/Installing"
+make distrib
+sudo make install
+echo -e "[+] All set go fuzz!"
